61c418b724ee69c7ca39f0f9b0c12173a1194f67
kajiwara3 <kajiwara3@gmail.com>
2012-07-05 23:12:55 +0900

    libvirtの検証用コードを追加した

--- app/controllers/vps_management_controller.rb
+++ app/controllers/vps_management_controller.rb
@@ -4,6 +4,14 @@ class VpsManagementController < ApplicationController
 
   # 一覧表示アクション。
   def index
+    begin
+      require 'libvirt'
+      url = "qemu+tcp://root@192.168.0.140:16509/system"
+      conn = Libvirt::open(url)
+      puts conn.list_defined_domains;
+    rescue => e
+      raise e.class, "#{e.message} -> piyo ", e.backtrace
+    end
     @server_list = current_partner.private_servers.
                         order("id").
                         paginate(page: params[:page], per_page: 5)
