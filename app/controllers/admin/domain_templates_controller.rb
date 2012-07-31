bd4115a92214ddd18aeaf32db2efe87b190217dd
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-07-31 18:03:36 +0900

    xmlファイルのアップロードフォームを追加した

--- app/controllers/admin/domain_templates_controller.rb
+++ app/controllers/admin/domain_templates_controller.rb
@@ -1,2 +1,30 @@
+#coding: utf-8
+# 管理者によるドメインのひな形登録用モジュール
 class Admin::DomainTemplatesController < ApplicationController
+  # 登録済みドメインテンプレートの一覧表示用アクション。
+  def index
+    @domain_list = DomainTemplate.order("id").
+      paginate(page: params[:page], per_page: 5)
+  end
+
+  # 登録済みドメインテンプレートの詳細表示アクション。
+  def show
+    @domain = DomainTemplate(params[:id])
+  end
+
+  # ドメインテンプレートの登録画面表示アクション。
+  def new
+    @domain = DomainTemplate.new
+  end
+
+  # ドメインテンプレートの登録アクション
+  def create
+    logger.debug(params[:domain_template])
+    @domain = DomainTemplate.new(params[:domain_template])
+    if @domain.save
+      redirect_to :admin_domain_template, notice: "ドメインテンプレートを追加しました"
+    else
+      render "new"
+    end
+  end
 end
