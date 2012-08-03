43f1a81118deb50a1ae3359f885b4cdff0ddc2c7
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-08-03 14:00:03 +0900

    xmlテンプレートの新規編集アクションと単体テストを実装し た

--- app/models/domain_template.rb
+++ app/models/domain_template.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 class DomainTemplate < ActiveRecord::Base
+  attr_accessible :name, :xml_data
   validates :name, presence: true, length: { maximum: 50 }
-  validates :xml_data, presence: true
+  validates :xml_data, presence: { on: :create }
 end
