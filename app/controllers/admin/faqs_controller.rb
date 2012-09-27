03ec8e4e898b5fc43de1eacbac93000ef064550e
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-09-27 11:19:30 +0900

    admin faqコントローラーのコメントを追加した (refs #118 )

--- app/controllers/admin/faqs_controller.rb
+++ app/controllers/admin/faqs_controller.rb
@@ -2,22 +2,24 @@
 class Admin::FaqsController < Admin::Base
   layout "admin_application"
 
-  # GET /admin/faqs
   # 一覧表示アクション。
   def index
     @faq_list = Faq.order(:id).
       paginate(page: params[:page], per_page: 5)
   end
 
+  # 詳細表示アクション、
   def show
     @faq = Faq.find(params[:id])
   end
 
+  # 編集画面表示アクション。
   def edit
     @faq = Faq.find(params[:id])
     @faq_categories = Admin::FaqCategory.order(:id)
   end
 
+  # 更新アクション。
   def update
     @faq = Faq.find(params[:id])
     @faq.assign_attributes params[:faq]
@@ -29,13 +31,13 @@ class Admin::FaqsController < Admin::Base
     redirect_to :admin_faq, notice: "FAQを更新しました"
   end
 
+  # 削除アクション。
   def destroy
     @faq = Faq.find params[:id]
     if !@faq.destroy
       flash[:alert] = "FAQ ID: [#{@faq.id}] の削除に失敗しました"
       return render "show"
     end
-
     redirect_to :admin_faqs, notice: "FAQ ID: [#{@faq.id}] を削除しました"
   end
 end
