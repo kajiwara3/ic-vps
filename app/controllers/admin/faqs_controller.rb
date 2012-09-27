b00fb400013ba129fbd69909e140b4c1c5637d59
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-09-27 11:17:32 +0900

    管理者用のFAQの編集／削除機能を実装した (refs #118 )

--- app/controllers/admin/faqs_controller.rb
+++ app/controllers/admin/faqs_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Admin::FaqsController < Admin::Base
   layout "admin_application"
 
@@ -14,5 +15,27 @@ class Admin::FaqsController < Admin::Base
 
   def edit
     @faq = Faq.find(params[:id])
+    @faq_categories = Admin::FaqCategory.order(:id)
+  end
+
+  def update
+    @faq = Faq.find(params[:id])
+    @faq.assign_attributes params[:faq]
+
+    if !@faq.save
+      @faq_categories = Admin::FaqCategory.order(:id)
+      return render "edit"
+    end
+    redirect_to :admin_faq, notice: "FAQを更新しました"
+  end
+
+  def destroy
+    @faq = Faq.find params[:id]
+    if !@faq.destroy
+      flash[:alert] = "FAQ ID: [#{@faq.id}] の削除に失敗しました"
+      return render "show"
+    end
+
+    redirect_to :admin_faqs, notice: "FAQ ID: [#{@faq.id}] を削除しました"
   end
 end
--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,3 +1,8 @@
 class Faq < ActiveRecord::Base
   belongs_to :admin_faq_category, class_name: "Admin::FaqCategory"
+
+  # バリデーションメソッド
+  validates :admin_faq_category_id, presence: true
+  validates :question, presence: true
+  validates :answer, presence: true
 end
