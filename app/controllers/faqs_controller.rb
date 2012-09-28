a89b8d616a0bd21850edc0e228a320219a93f0a9
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-09-28 14:21:03 +0900

    パートナー用のFAQの一覧機能の不具合を修正した (refs #118 )

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -2,8 +2,7 @@ class FaqsController < ApplicationController
   # 一覧アクション。
   def index
     faq_category_id = params[:faq_category_id]
-    if faq_category_id && faq_category
-      faq_category = Admin::FaqCategory.find(faq_category_id)
+    if faq_category_id
       @faq_list = Faq.where("admin_faq_category_id = ?", faq_category_id).order(:id).
         paginate(page: params[:page], per_page: 5)
       return
