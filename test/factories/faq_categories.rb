38fde3a7ad73e3d7837be1ebc4401d5609802469
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-10-11 11:33:20 +0900

    FAQカテゴリーモデルのUnitTestを実装した (refs #120 )

--- test/factories/faq_categories.rb
+++ test/factories/faq_categories.rb
@@ -1,6 +1,7 @@
+# coding: utf-8
 # Read about factories at http://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :faq_category do
+    name "テストカテゴリ"
   end
 end
--- test/unit/faq_category_test.rb
+++ test/unit/faq_category_test.rb
@@ -1,7 +1,18 @@
+# coding: utf-8
 require 'test_helper'
-
 class FaqCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "faq_category" do
+    faq_category = Factory(:faq_category)
+    assert_equal "テストカテゴリ", faq_category.name
+  end
+
+  test "presence" do
+    faq_category = FaqCategory.new
+    assert faq_category.invalid?
+    assert faq_category.errors.include?(:name)
+  end
 end
