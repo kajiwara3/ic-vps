25e5854c6e854de06c4ef78d12230512ec8562d8
seiji akatsuka <info.akatsuka@gmail.com>
2013-01-30 17:13:31 +0900

    ページネーションをKaminariに変更し、Ajaxによるレンダリング処理を追加した

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,8 +2,12 @@
 class NotificationsController < ApplicationController
   # お知らせの一覧表示アクション。
   def index
-    @notifications =Notification.order("id desc").
-      paginate(page: params[:page], per_page: 10)
+    @notifications = Kaminari.paginate_array(Notification.order "id desc").
+                      page(params[:page]).per(5)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # お知らせの詳細表示アクション。
