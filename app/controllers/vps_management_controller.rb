8ef1780589c192724deacc6fc430e8462fbd2a7a
seiji akatsuka <info.akatsuka@gmail.com>
2013-02-05 15:34:17 +0900

    wii_paginateからKamiariに変更し、Ajaxによる表示を追加した

--- app/controllers/vps_management_controller.rb
+++ app/controllers/vps_management_controller.rb
@@ -5,11 +5,12 @@ class VpsManagementController < ApplicationController
 
   # 一覧表示アクション。
   def index
-    @defined_domain_list = list_vps
-    @active_domain_list = active_vps_list
-    @server_list = current_partner.private_servers.
-                        order("id").
-                        paginate(page: params[:page], per_page: 5)
+    @server_list = Kaminari.paginate_array(current_partner.private_servers.
+                        order("id")).page(params[:page]).per(5)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # 詳細表示。
