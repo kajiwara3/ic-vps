cc2537b2ed818caad7cffb5e205f2a721ac09c06
seiji akatsuka <info.akatsuka@gmail.com>
2013-02-13 14:21:58 +0900

    Deviseに合わせて、管理者用のログインリンクを修正した

--- app/controllers/admin/administrators_controller.rb
+++ app/controllers/admin/administrators_controller.rb
@@ -1,6 +1,5 @@
 # coding: utf-8
 class Admin::AdministratorsController < Admin::Base
-  before_filter :authenticate_admin_administrator!
   add_crumb "管理者TOP", "/admin/administrators/"
 
   # indexアクション
--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -2,22 +2,9 @@
 class Admin::Base < ApplicationController
   #before_filter :authorize
   #before_filter :admin_login_required
-  #skip_before_filter :record
+  before_filter :authenticate_admin_administrator!
+  skip_before_filter :record
   layout "admin_application"
   # パンくず設定
   add_crumb 'HOME', "/admin"
-
-  # リクエストごとに認証を行うアクション。
-  private
-  def authorize
-    if session[:administrator_id]
-      @current_administrator = Administrator.find(session[:administrator_id])
-      session.delete(:administrator_id) unless @current_administrator
-    end
-  end
-
-  # 管理者ではない場合にログイン画面にリダイレクトするアクション。
-  def admin_login_required
-    redirect_to :admin_root unless @current_administrator
-  end
 end
