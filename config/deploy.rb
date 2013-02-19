ef225a30a522585d3d81336a59fe50f93388e152
seiji akatsuka <info.akatsuka@gmail.com>
2013-02-19 17:26:02 +0900

    capistranoでのデプロイタスクに、preconpileを追加した

--- config/deploy.rb
+++ config/deploy.rb
@@ -91,6 +91,17 @@ namespace :deploy do
       system "cap deploy:set_permissions"
     end
   end
+
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
 end
 
 desc "install the necessary prerequisites"
