aedc94ae632df633ff9b03e049cae073e2f03bb2
seiji akatsuka <info.akatsuka@gmail.com>
2013-02-27 16:48:45 +0900

    ユーザーによるアカウント登録を無効にした

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -10,7 +10,7 @@ class Partner < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
