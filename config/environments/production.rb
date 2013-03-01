810a82ed9ab95141d181a7013e4c49e312be8143
seiji akatsuka <info.akatsuka@gmail.com>
2013-03-01 17:32:26 +0900

    本番環境用のKVMの設定を追加した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,10 @@ IcVps::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Hypervisor uri
+  config.hypervisor_uri = "qemu+tcp://root@localhost:16509/system"
+
+  #application host name
+  config.host_name = "ic-vps"
 end
