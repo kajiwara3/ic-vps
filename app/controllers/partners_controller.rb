2023d894983f19428c7024d4d5fea34996dd49bd
seiji akatsuka <info.akatsuka@gmail.com>
2013-03-01 17:03:20 +0900

    パスワードを更新しない場合の処理をController側で行うようにした(refs #199 )

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -13,12 +13,22 @@ class PartnersController < ApplicationController
 
   def update
     @partner = current_partner
+    delete_unchange_password_params
     @partner.assign_attributes(params[:partner])
 
     if @partner.save
       redirect_to "/partner", notice: "ご登録情報を更新しました"
     else
       render "edit"
+    end
+  end
+
+  private
+  def delete_unchange_password_params
+    if params[:partner][:password].blank? &&
+       params[:partner][:password_confirmation].blank?
+       params[:partner].delete :password
+       params[:partner].delete :password_confirmation
     end
   end
 end
