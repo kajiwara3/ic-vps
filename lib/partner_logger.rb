27e2b5399dfa63120cb09ff85b74bda59d42570b
seiji akatsuka <info.akatsuka@gmail.com>
2013-03-01 16:31:17 +0900

    Partnerオブジェクトがnilだった場合の処理を追加した(refs #193 )

--- lib/partner_logger.rb
+++ lib/partner_logger.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 module PartnerLogger
   def record
+    return if current_partner.nil?
     @partner_operation_log = PartnerOperationLog.new(partner_id: current_partner.id,
                                                      url: request.fullpath,
                                                      ip_address: request.remote_ip,
